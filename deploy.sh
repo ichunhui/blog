7a2144e22f4a530249eb3898e381eb44c628c582
jimmy <hustzch@163.com>
2022-04-15 20:10:28 +0800

    update action

--- deploy.sh
+++ deploy.sh
@@ -25,22 +25,23 @@ cd ${ROOT_DIR}/.temp
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-if [[ ${GITHUB_TOKEN} && ${GITEE_TOKEN} ]]; then
+# if [[ ${GITHUB_TOKEN} && ${GITEE_TOKEN} ]]; then
+if [[ ${GITHUB_TOKEN} ]]; then
   msg='自动部署'
-  GITHUB_URL=https://dunwu:${GITHUB_TOKEN}@github.com/dunwu/blog.git
-  GITEE_URL=https://turnon:${GITEE_TOKEN}@gitee.com/turnon/blog.git
-  git config --global user.name "dunwu"
-  git config --global user.email "forbreak@163.com"
+  GITHUB_URL=https://ichunhui:${GITHUB_TOKEN}@github.com/ichunhui/blog2.git
+  # GITEE_URL=https://turnon:${GITEE_TOKEN}@gitee.com/turnon/blog.git
+  git config --global user.name "jimmy"
+  git config --global user.email "hustzch@163.com"
 else
   msg='手动部署'
-  GITHUB_URL=git@github.com:dunwu/blog.git
-  GITEE_URL=git@gitee.com:turnon/blog.git
+  GITHUB_URL=https://github.com/ichunhui/blog2.git
+  # GITEE_URL=git@gitee.com:turnon/blog.git
 fi
 git init
 git add -A
 git commit -m "${msg}"
 # 推送到github gh-pages分支
 git push -f "${GITHUB_URL}" master:gh-pages
-git push -f "${GITEE_URL}" master:gh-pages
+# git push -f "${GITEE_URL}" master:gh-pages
 
 rm -rf ${ROOT_DIR}/.temp
